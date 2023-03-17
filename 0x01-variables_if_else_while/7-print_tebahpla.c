dda1e47fb29c7e28b9b8717ee0ba513044ce8953
Keely001 <thindiuke@gmail.com>
2023-03-17 14:51:49 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Printing char
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; a--)
+	putchar(c);
+	putchar("\n");
+
+	return (0);
+}
