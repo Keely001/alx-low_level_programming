de69566bef694a3aab6fef581036c033daddc4b7
Keely001 <thindiuke@gmail.com>
2023-03-17 15:11:07 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Base 16 numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	char b
+	
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+
+	for (b = 'a'; b <= 'f'; b++)
+	putchar(b);
+	putchar('\n');
+
+	return (0);
+}
