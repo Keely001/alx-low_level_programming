c37360286b4c409aae46a0691f9474b2c573ddba
Keely001 <thindiuke@gmail.com>
2023-03-22 06:46:40 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,25 +5,25 @@
  */
 void times_table(void)
 {
-	int i, j;
-	int curValue;
+	int i, j, k;
 
-	for (i = 0; i <= 0; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0;j <= 9;j++)
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
 		{
-			curValue = i*j;
-			if ( curValue >= 9)
-			{
-				_putchar(curValue = '0');
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+
+			k = i * j;
+
+			if (k <= 9)
 				_putchar(' ');
-			}
 			else
-			{
-				_putchar(curValue + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
+				_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
 		_putchar('\n');
+	}
+}
