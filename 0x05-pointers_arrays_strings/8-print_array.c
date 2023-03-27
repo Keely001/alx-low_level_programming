863794f1946aab9ebabff24220612594183e60de
Keely001 <thindiuke@gmail.com>
2023-03-27 13:24:53 -0700

    void print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints an array in order
+ * @a: array
+ * @n: no. of elements
+ * Return: returns nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0;i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d",a[i]);
+		else 
+			printd("%d, ",a[i]);
+	}
+	printf("\n");
+}
