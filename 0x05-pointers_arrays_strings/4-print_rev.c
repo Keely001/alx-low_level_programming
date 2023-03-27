ba633c1b52f2f23630f203a5d19702c655a05366
Keely001 <thindiuke@gmail.com>
2023-03-27 12:04:08 -0700

    puts2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 	int i = 0;
 	int j;
 
-	while (*s != '\n')
+	while (*s != '\0')
 	{
 		i++;
 		s++;
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters
+ * @str: string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int a;
+
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	for (a = 0; a < i; a++)
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
