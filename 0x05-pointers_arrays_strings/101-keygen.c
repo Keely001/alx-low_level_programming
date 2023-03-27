e4d4ae0a59702be954069d6360fb5c711b30aaa3
Keely001 <thindiuke@gmail.com>
2023-03-27 13:05:35 -0700

    101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - password generator.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int password[100];
+	int i, total, n;
+
+	total = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 78;
+		total += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - total) - '0' < 78)
+		{
+			n = 2772 - total - '0';
+			total += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
