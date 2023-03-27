7288428fe58881f6b93a490b7a9b24b321eb5844
Keely001 <thindiuke@gmail.com>
2023-03-27 14:10:49 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,8 @@ void print_rev(char *s)
 	int i = 0;
 	int j;
 
-	while (*s != '\0')
-	{
+	while (*(s + i) != '\0')
 		i++;
-		s++;
-	}
-	s--;
 	for (j = i - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
