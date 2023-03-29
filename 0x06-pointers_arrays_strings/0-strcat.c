5388235df2f04c82a27d5927848fdf628affb1c3
Keely001 <thindiuke@gmail.com>
2023-03-29 08:38:51 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,37 +7,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int count, count2, totalL;
-	int i;
+	int count, count2;
 
 	count = 0;
 	count2 = 0;
-	totalL = 0;
 
 	while (*dest != '\0')
 	{
 		count++;
-		dest++;
 	}
 	while (*src != '\0')
 	{
+		*(dest + count) = *(src + count2);
+		count++;
 		count2++;
-		src++;
 	}
-	totalL = count + count2;
-	for (i = 0; i <= totalL; i++)
-	{
-		if (i >= 0 && i < count)
-			*(dest + i) = *(dest + i);
-		else
-		{
-			while (*src != '\0')
-			{
-				*(dest + i) = *src;
-				src++;
-			}
-			*(dest + i) = '\0';
-		}
-	}
+	dest[count] = '\0';
 	return (dest);
 }
