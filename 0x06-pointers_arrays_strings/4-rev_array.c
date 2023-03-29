2524c2a383f9e0b1825fa899ea05c4764df53695
Keely001 <thindiuke@gmail.com>
2023-03-29 12:58:20 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * reverse_array - reversing an array
+ * @a: the array
+ * @n: no. of elements
+ * Return: none
+ */
+void reverse_array(int *a, int n)
+{
+	int midNum;
+	int i;
+	int count;
+	int reserve;
+
+	count = 0;
+	midNum = 0;
+	if (n % 2 == 0)
+		midNum = n / 2;
+	else
+		midNum = (n / 2) - 2;
+	i = 0;
+	while (a[i] != '\0')
+	{
+		count++;
+	}
+	count--;
+	for (j = 0; j <= midNum; j++)
+	{
+		reserve = a[count];
+		a[count] = a[j];
+		a[j] = reserve
+		count--;
+	}
+}
