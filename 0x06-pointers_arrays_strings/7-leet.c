763584e2b86410f3f7d98706cfe6dd44e988b546
Keely001 <thindiuke@gmail.com>
2023-03-30 03:22:51 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,20 @@
  */
 char *leet(char *a)
 {
-	int i;
-	int j;
-
-	char string1[] = "aAeEoOtTlL";
-	char string2[] = "4433007711";
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; i++)
+		for (j = 0; j < 10; j++)
 		{
-			if (a[i] == string1[j])
+			if (a[i] == s1[j])
 			{
-				a[i] = string2[j];
+				a[i] = s2[j];
 			}
 		}
 	}
 	return (a);
+
 }
