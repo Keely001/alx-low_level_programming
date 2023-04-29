d9f568ea62ce0b52eed438717556f22ce8e30c07
Keely001 <thindiuke@gmail.com>
2023-04-29 11:41:57 -0700

    task 0 (to print ints)

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * struct list - singly linked node
+ * @n: number
+ * @next: aaddress of the next node
+ */
+typedef struct list
+{
+	unsigned int n;
+	struct list *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
