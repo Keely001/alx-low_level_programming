c74d99e56a96745c92a5b6694394fef87c167251
Keely001 <thindiuke@gmail.com>
2023-05-01 03:57:05 -0700

    task 9 corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = new;
+		return (new);
 	}
 	while (i < idx)
 	{
@@ -36,5 +37,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			pos = pos->next;
 		i++;
 	}
-	return (new);
+	return (NULL);
 }
