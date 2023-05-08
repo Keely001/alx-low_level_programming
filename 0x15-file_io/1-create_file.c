3d5a9b6490617aaef8825ba84f2f47ce54e2d3e4
Keely001 <thindiuke@gmail.com>
2023-05-08 08:11:13 -0700

    Task 1 twitching

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,21 +10,27 @@
 int create_file(const char *filename, char *text_content)
 {
 	int length = 0;
-	int checker;
+	int checker = 0;
+	int w = 0;
+	const char *text_ptr = text_content;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (*text_content)
+		while (*text_ptr)
 		{
 			length++;
-			text_content++;
+			text_ptr++;
 		}
 	}
 	checker = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if ((write(checker, text_content, length)) == -1 || checker == -1)
+	w = write(checker, text_content, length);
+	if (w == -1 || checker == -1)
+	{
+		close(checker);
 		return (-1);
+	}
 	close(checker);
 
 	return (1);
